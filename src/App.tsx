cf909c958a92b4298e2907545948c441cd595fa1
Amantay <amantay.sv@gmail.com>
2024-06-03 17:57:34 +0600

    mobile

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ function App() {
   const tables = new Array(6).fill(null)
   return (
     <div className='flex flex-col p-2 gap-2 min-h-screen'>
-      <div className='mx-auto border border-black py-2 px-20 min-w-[25%] text-center'>Экран</div>
+      <div className='hidden sm:block mx-auto border border-black py-2 px-20 min-w-[25%] text-center'>
+        Экран
+      </div>
 
-      <div className='flex-1 flex justify-between items-start'>
+      <div className='flex-1 flex-col-reverse sm:flex-row flex justify-between items-start'>
         <div className='flex flex-wrap'>
           {tables.map((_, index) => (
-            <div className='p-1 w-1/2' key={index + 7}>
+            <div className='p-1 w-full sm:w-1/2' key={index + 7}>
               <Table id={index + 7} />
             </div>
           ))}
@@ -17,14 +19,14 @@ function App() {
 
         <div className='flex flex-wrap'>
           {tables.map((_, index) => (
-            <div className='p-1 w-1/2' key={index + 1}>
+            <div className='p-1 w-full sm:w-1/2' key={index + 1}>
               <Table id={index + 1} />
             </div>
           ))}
         </div>
       </div>
 
-      <div className='flex justify-center gap-x-36 items-end'>
+      <div className='hidden sm:block flex justify-center gap-x-36 items-end'>
         <div className='border border-black py-14 px-2 text-center'>вход</div>
         <div className='border border-black py-2 px-20 min-w-[20%] text-center'>
           стол молодоженов
